8175a5580f03be78ea14a171efd8de4906abf3e0
Govind S <govindsys1008@gmail.com>
2026-04-19 18:44:58 +0530

    fix

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 from typing import Optional
 class InputSchema(BaseModel):
     file_name:str
-    file_type:Optional[str]="pdf"
+    content_type:Optional[str]="pdf"
 

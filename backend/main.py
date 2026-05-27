ac31aaa50adf71df1a5a6343cee163b2f66219bb
Govind <govindsys1008@gmail.com>
2026-05-27 22:29:24 +0530

    extraction and hashing code done

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import os, boto3
 from dotenv import load_dotenv
 from schemas import InputSchema
 load_dotenv()
+from utils.extractor import extract, extract_ocr, extract_csv, hash_text
 import uuid
 s3= boto3.client('s3', region_name=os.getenv("S3_REGION"), aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
 bucket=os.getenv("S3_BUCKET_NAME")
@@ -23,3 +24,14 @@ def upl(payload: InputSchema):
     )
     return {"upload_url": pres, "file_key": key}
 
+@app.post("/extract")
+def extr(payload: ExtractSchema):
+    file_key= payload.file_key
+    response= s3.get_object(Bucket=bucket, key=file_key)
+    file_bytes=response["Body"].read()
+    text= extract(response)
+    if len(text.strip())< 100:
+        text = extract_ocr(text)
+    #TODO: CSV Parsing code
+    text_hash=hash_text(text)
+    return {"hashed_text": text_hash}

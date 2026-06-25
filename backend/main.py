0e178e7a8fa38c37f0e7d40af7dbf22015d97fc7
Govind <govindsys1008@gmail.com>
2026-06-25 15:52:05 +0530

    frontend fix (done by Satyam)

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ bucket=os.getenv("S3_BUCKET_NAME")
 origins = ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://127.0.0.1', '0.0.0.0']
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins = origins,
+                   allow_origins = ["*"],
                    allow_credentials = True,
                    allow_methods = ['*'],
                    allow_headers = ['*'])

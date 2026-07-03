66d88845cd1d4bee5362a35c3ca552363fdc77b0
satyam-1906 <scientificsatyam1906@gmail.com>
2026-07-03 15:17:02 +0530

    made a console and dashboard

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ redis_client=Redis(
 )
 bucket=os.getenv("S3_BUCKET_NAME")
 
-origins = ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://127.0.0.1', '0.0.0.0']
+origins = ['http://localhost:5500', 'http://127.0.0.1:5501', 'http://127.0.0.1', '0.0.0.0']
 
 app.add_middleware(CORSMiddleware,
                    allow_origins = origins,
